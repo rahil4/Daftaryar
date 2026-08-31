9133f1b3d57b014e2b21b1905d5cb66d2b92b596
rahil <r.rahil@gmail.com>
2026-09-01 01:39:04 +0330

    رفع خطای واقعی: late final به‌جای final برای _selectedRoles - حذف late باعث خطای دسترسی زودهنگام به widget در Initializer شده بود

--- lib/navigation.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// کلید ناوبری سراسری — برای بازکردن صفحه از داخل کدهایی که به BuildContext
-/// دسترسی مستقیم ندارند (مثل هندلر لمس نوتیفیکیشن)
-final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
